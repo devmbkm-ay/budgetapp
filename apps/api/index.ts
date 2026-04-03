329dd0d0d25737677886cdacb5a602fe32d29775
ricardomb94 <aymardmb@gmail.com>
2026-04-03 02:34:05 +0200

    feat(backend):install and use swagger module

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,7 +1,11 @@
 import { Elysia } from "elysia";
+import { swagger } from "@elysiajs/swagger";
 
 const app = new Elysia()
+    .use(swagger()) // Register the Swagger plugin
+    .get('/health', () => ({ status: 'OK', uptime: process.uptime() }))
     .get("/", () => "Hello budget app API!")
+
     .listen(3001)
 
 console.log(`Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
